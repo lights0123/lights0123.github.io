e3fad1d35f2bd7adb1471b3dcd47060567b075b8
lights0123 <developer@lights0123.com>
2020-10-06 17:07:11 -0400

    Update n-link

--- src/pages/NLink.vue
+++ src/pages/NLink.vue
@@ -8,7 +8,26 @@
     </p>
     <br>
     <h2 class="text-3xl">Download {{ releases ? releases.version : '' }}</h2>
-    <div v-if="releases" class="grid xl:grid-cols-7 md:grid-cols-2 gap-4">
+    <div v-if="releases" class="grid xl:grid-cols-6 md:grid-cols-2 gap-4">
+      <a href="https://n-link.lights0123.com" target="_blank" rel="noopener"
+         class="xl:col-span-3 py-3 bg-gray-600 flex justify-center items-center text-white"
+         :class="webUSB && 'bg-gradient'">
+        <Windows class="w-8 h-8 mr-4" />
+        <div class="text-center">
+          <p class="font-bold">Open on the Web</p>
+          <p v-if="webUSB">Chrome-based browsers</p>
+          <p v-else>Requires a Chrome-based browser</p>
+        </div>
+      </a>
+      <a class="xl:col-span-3 py-3 bg-blue-700 flex justify-center items-center text-white"
+         href="https://github.com/lights0123/n-link"
+         target="_blank" rel="noopener">
+        <Github class="w-8 h-8 mr-4" />
+        <div class="text-center">
+          <p class="font-bold">Source code</p>
+          <p>GPL v3.0</p>
+        </div>
+      </a>
       <a :href="releases.msi" target="_blank" rel="noopener"
          class="xl:col-span-2 py-3 bg-blue-700 flex justify-center items-center text-white">
         <Windows class="w-8 h-8 mr-4" />
@@ -43,20 +62,23 @@
           </div>
         </a>
       </div>
-      <a class="py-3 bg-blue-700 flex justify-center items-center text-white"
-         href="https://github.com/lights0123/n-link"
-         target="_blank" rel="noopener">
-        <Github class="w-8 h-8 mr-4" />
+    </div>
+    <div v-else class="my-12 grid sm:grid-cols-2 gap-4">
+      <a href="https://n-link.lights0123.com" target="_blank" rel="noopener"
+         class="py-3 bg-gray-600 flex justify-center items-center text-white"
+         :class="webUSB && 'bg-gradient'">
+        <Windows class="w-8 h-8 mr-4" />
         <div class="text-center">
-          <p class="font-bold">Source code</p>
-          <p>GPL v3.0</p>
+          <p class="font-bold">Open on the Web</p>
+          <p v-if="webUSB">Chrome-based browsers</p>
+          <p v-else>Requires a Chrome-based browser</p>
         </div>
       </a>
+      <a href="https://github.com/lights0123/n-link/releases/latest" target="_blank"
+         rel="noopener" class="px-4 py-6 bg-blue-700 text-white flex justify-center items-center">
+        Download on GitHub
+      </a>
     </div>
-    <div v-else class="my-12"><a href="https://github.com/lights0123/n-link/releases/latest" target="_blank"
-                                 rel="noopener" class="px-4 py-6 bg-blue-700 text-white">
-      Download on GitHub
-    </a></div>
     <h2 class="text-3xl mt-4">Screenshots</h2>
     <div class="lg:grid grid-cols-2 gap-8">
       <figure>
@@ -94,14 +116,10 @@
         <a href="https://github.com/Vogtinator/libnspire/issues/1" target="_blank"
            rel="noopener">Vogtinator/libnspire#1</a>
       </p></li>
-      <li><p>
-        Creating top-level directories: blocked by
-        <a href="https://github.com/Vogtinator/libnspire/issues/2" target="_blank"
-           rel="noopener">Vogtinator/libnspire#2</a>
-      </p></li>
       <li>CLI on Windows</li>
       <li>One-click Ndless installation</li>
       <li>Automatic OS downloading</li>
+      <li>Drag & Drop</li>
     </ul>
     <h2 class="text-3xl mt-4">Installation Instructions</h2>
     <h3 class="text-2xl mt-4" id="windows">Windows</h3>
@@ -183,6 +201,13 @@ const description = 'Free, cross-platform, CX-II compatible computer linking pro
 })
 export default class NLink extends Vue {
   releases: { version: string; msi: string; dmg: string; appimage: string; deb: string } | null = null;
+  // releases: { version: string; msi: string; dmg: string; appimage: string; deb: string } | null = {
+  //   version: '1',
+  //   msi: 'a',
+  //   dmg: 'a',
+  //   appimage: 'a',
+  //   deb: 'a',
+  // };
 
   async mounted() {
     const { tag_name, assets } = await (await fetch('https://api.github.com/repos/lights0123/n-link/releases/latest')).json() as { tag_name: string; assets: Asset[] };
@@ -200,10 +225,25 @@ export default class NLink extends Vue {
     };
     console.log(this.releases);
   }
+
+  get webUSB() {
+    return process.isClient && !!(navigator as any).usb;
+  }
 };
 </script>
 <style lang="scss" scoped>
 ul {
   @apply list-outside list-disc ml-4;
+}
+
+.bg-gradient {
+  background: linear-gradient(135deg, #a13de0 20%, #0b75e0 100%);
+  @apply shadow-2xl rounded;
+  transition: transform 0.2s, border-radius 0.2s;
+
+  &:hover {
+    transform: scale(1.01);
+    @apply rounded-lg;
+  }
 }
 </style>
