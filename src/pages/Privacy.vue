f1c3ef7dab4f8abf6af2316fc3bbb4ca5d59607d
lights0123 <developer@lights0123.com>
2021-01-09 22:28:58 -0500

    Add privacy policy

---
+++ src/pages/Privacy.vue
@@ -0,0 +1,103 @@
+<template>
+  <Layout>
+    <h1 class="mt-4">Privacy Policy</h1>
+    <p>This Privacy Policy contains types of information that is collected and recorded by this website and how we use
+      it.</p>
+    <p>If you have additional questions or require more information about our Privacy Policy, do not hesitate to
+      <g-link to="/about">contact me</g-link>
+      .
+    </p>
+    <p>This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards
+      to the information that they shared and/or collect in the website. This policy is not applicable to any
+      information collected offline or via channels other than this website. Our Privacy Policy was created with the
+      help of the <a href="https://www.privacypolicygenerator.info">Privacy Policy Generator</a> and the <a
+          href="https://www.generateprivacypolicy.com/#wizard">Free Privacy Policy Generator</a>.</p>
+    <h2>Consent</h2>
+    <p>By using our website, you hereby consent to our Privacy Policy and agree to its terms.</p>
+    <h2>Information we collect</h2>
+    <p>If you explicitly choose to contact me, I will have access to any information you choose to provide. This
+      information will only be used for replying, unless you explicitly authorize me to use it otherwise.</p>
+    <p>From normal browsing, the following information may be collected:</p>
+    <ul>
+      <li>IP Address</li>
+      <li>Browser and device information</li>
+      <li>Pages visited on this website</li>
+      <li>Interactions with this website</li>
+      <li>Referring page</li>
+    </ul>
+    The information passively collected is never shared or under the control of any third party, nor is it combined with
+    data gathered from other websites (i.e. I do not track you across websites that I do not own).
+    <h2>How we use your information</h2>
+    <p>We use the information we collect in various ways, including to:</p>
+    <ul>
+      <li>Ensure that users of all browsers are able to use the Website</li>
+      <li>Understand usage patterns of the website</li>
+      <li>Identify where users come from</li>
+    </ul>
+    <h2>Log Files</h2>
+    <p>No log files are collected.</p>
+    <h2>Data Retention</h2>
+    <p>Collected data is retained for up to 1 year, unless you contact me to have it removed earlier.</p>
+    <h2>Cookies and Web Beacons</h2>
+    <p>Like any other website, Ben Schattinger uses 'cookies'. These cookies are used to store information including
+      visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used
+      to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other
+      information.</p>
+    <p>For more general information on cookies, please read <a href="https://www.cookieconsent.com/what-are-cookies/">"What
+      Are Cookies" from Cookie Consent</a>.</p>
+    <h2>CCPA Privacy Rights (Do Not Sell My Personal Information)</h2>
+    <p>Under the CCPA, among other rights, California consumers have the right to:</p>
+    <p>Request that a business that collects a consumer's personal data disclose the categories and specific pieces of
+      personal data that a business has collected about consumers.</p>
+    <p>Request that a business delete any personal data about the consumer that a business has collected.</p>
+    <p>Request that a business that sells a consumer's personal data, not sell the consumer's personal data.</p>
+    <p>If you make a request, we have one month to respond to you. If you would like to exercise any of these rights,
+      please
+      <g-link to="/about">contact me</g-link>
+      .
+    </p>
+    <h2>GDPR Data Protection Rights</h2>
+    <p>We would like to make sure you are fully aware of all of your data protection rights. Every user is entitled to
+      the following:</p>
+    <p>The right to access – You have the right to request copies of your personal data. We may charge you a small fee
+      for this service.</p>
+    <p>The right to rectification – You have the right to request that we correct any information you believe is
+      inaccurate. You also have the right to request that we complete the information you believe is incomplete.</p>
+    <p>The right to erasure – You have the right to request that we erase your personal data, under certain
+      conditions.</p>
+    <p>The right to restrict processing – You have the right to request that we restrict the processing of your personal
+      data, under certain conditions.</p>
+    <p>The right to object to processing – You have the right to object to our processing of your personal data, under
+      certain conditions.</p>
+    <p>The right to data portability – You have the right to request that we transfer the data that we have collected to
+      another organization, or directly to you, under certain conditions.</p>
+    <p>If you make a request, we have one month to respond to you. If you would like to exercise any of these rights,
+      please
+      <g-link to="/about">contact me</g-link>
+      .
+    </p>
+    <h2>Children's Information</h2>
+    <p>Another part of our priority is adding protection for children while using the internet. We encourage parents and
+      guardians to observe, participate in, and/or monitor and guide their online activity.</p>
+    <p>Ben Schattinger does not knowingly collect any Personal Identifiable Information from children under the age of
+      13. If you think that your child provided this kind of information on our website, we strongly encourage you to
+      <g-link to="/about">contact me</g-link>
+      immediately and we will do our best efforts to promptly remove such information from our records.
+    </p>
+  </Layout>
+</template>
+
+<script>
+
+export default {
+  metaInfo: {
+    title: 'Privacy Policy',
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  @apply list-outside list-disc ml-4;
+}
+</style>
