bc8db23b422cbf3425f107f2cbf12f7401f6ae03
lights0123 <developer@lights0123.com>
2021-01-09 19:31:43 -0500

    Include snapchat button

---
+++ src/components/ShareOnSnap.vue
@@ -0,0 +1,35 @@
+<template>
+  <a target="_blank" :href="snapUrl" rel="noreferrer, noopener">
+    <slot v-if="$slots.default" />
+    <div v-else class="bg-brand-snap inline-flex p-2 rounded-full items-center">
+      <Snapchat class="h-6 ml-1 mr-2" />
+      <ShareOnSnapText class="h-6 mr-2 mt-1 w-auto" />
+    </div>
+  </a>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import Snapchat from '@/assets/snapchat.svg';
+import ShareOnSnapText from '@/assets/share-on-snap.svg';
+
+@Component({components:{Snapchat, ShareOnSnapText}})
+export default class ShareOnSnap extends Vue {
+  @Prop({ type: String, required: true }) url!: string;
+
+  mounted() {
+    console.log(this);
+  }
+
+  get snapUrl() {
+    return `https://www.snapchat.com/scan?attachmentUrl=${encodeURIComponent(this.url)}`;
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.image-wrapper::v-deep .g-image {
+  max-height: 300px;
+  object-fit: scale-down;
+}
+</style>
