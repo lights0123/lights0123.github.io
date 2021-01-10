b9ffe175bc67ae999d7edeedea333c4cd95154ee
lights0123 <developer@lights0123.com>
2021-01-09 22:32:36 -0500

    Add privacy policy...and make it visible

--- src/layouts/PageFooter.vue
+++ src/layouts/PageFooter.vue
@@ -2,7 +2,7 @@
 	<footer class="bg-ui-sidebar p-8 mt-6">
 		<div class="container mx-auto grid md:grid-cols-2 md:gap-4">
 			<div>
-				© {{year}} Ben Schattinger
+				© {{year}} Ben Schattinger <g-link to="/privacy" class="border-b border-ui-blockquote ml-2">Privacy</g-link>
 			</div>
 			<div>Suggestions? <a class="border-b-2 text-ui-emph border-ui-border hover:border-ui-primary" rel="noopener" target="_blank" href="https://github.com/lights0123/lights0123.github.io/issues/new">Submit an issue</a></div>
 		</div>
